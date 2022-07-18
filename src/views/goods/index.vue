0ef8d19aa2b314b30dddfdc2eab17986e8f8828e
zp <1947750414@qq.com>
2022-07-18 11:37:15 +0800

    变量重命名

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <el-select v-model="options.value" class="m-2" placeholder="教师名称" size="large" @change="reqCourse">
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+    <el-select v-model="teachersList.teacherId" class="m-2" placeholder="教师名称" size="large" @change="queryCourse">
+      <el-option v-for="item in teachersList" :key="item.teacherId" :label="item.teacherName" :value="item.teacherId" />
     </el-select>
-    <el-select v-model="courseList.value" placeholder="课程名称" size="large" clearable class="filter-item"
-      style="margin-left: 8px;width: 200px" @change="getParams">
-      <el-option v-for="item in courseList" :key="item.key" :label="item.label" :value="item.value" />
+    <el-select v-model="courseList.courseId" placeholder="课程名称" size="large" clearable class="filter-item"
+      style="margin-left: 8px;width: 200px">
+      <el-option v-for="item in courseList" :key="item.courseId" :label="item.courseName" :value="item.courseId" />
     </el-select>
     <el-button type="primary" style="margin-left:10px" size="large" @click="queryStudents">查询</el-button>
   </div>
   <div class="el_table">
     <el-card>
-      <el-table :data="tableData" border style="width: 100%" stripe>
+      <el-table :data="studentsTable" border style="width: 100%" stripe>
         <el-table-column prop="bid" label="班级编号" width="180" />
         <el-table-column prop="major" label="专业" width="180" />
         <el-table-column prop="sid" label="学号" width="180" />
@@ -20,7 +20,7 @@
         <el-table-column label="重新赋分">
           <template #default="{ row }">
             <template v-if="row">
-              <el-input v-model="row.newScore" class="edit-input" size="large" max="100"/>
+              <el-input v-model="row.newScore" class="edit-input" size="large" max="100" />
             </template>
           </template>
         </el-table-column>
@@ -38,84 +38,50 @@ import { getStudentsScore, postSubmitScore } from '../../api/student'
 import { getTeacherCourse, getTeachers } from '../../api/teacher'
 import { ElMessage } from 'element-plus'
 
-// const value = ref('')
-// const listLoading = ref(true)
-const params = ref('')
-const options = ref([])
-const courseList = ref([])
-const tableData = reactive([])
-const data = reactive([])
-// const scores = ref([])
-// 获取教师列表（选择器）
+const teachersList = ref([])
 const getTeacherList = async () => {
-  const result = await getTeachers()
-  // options.push(list.data)
-  result.data.map(function (obj) {
+  const { data } = await getTeachers()
+  // teachersList.push(list.data)
+  data.forEach(function (items) {
     const rObj = {}
-    rObj.value = obj.tid
-    rObj.label = obj.tname
-    options.value.push(rObj)
+    rObj.teacherId = items.tid
+    rObj.teacherName = items.tname
+    teachersList.value.push(rObj)
   })
 }
 
-// 查询教师对应的课程（选择器）
-const reqCourse = async (val) => {
+const courseList = ref([])
+
+const queryCourse = async (val) => {
   courseList.value = []
-  const { data } = await getTeacherCourse(val.toString())
-  // console.log(result.data)
-  data.forEach(function (obj) {
+  const { data } = await getTeacherCourse(val)
+  data.forEach(function (items) {
     const rObj = {}
-    // 班级编号
-    rObj.value = obj.class.bid + obj._id
-    // 课程名称
-    rObj.label = obj.course.cname
-    // 课程唯一id
-    rObj.cid = obj.course.cid
-    // 排课表的id
-    rObj.id = obj._id
+    rObj.courseName = items.course.cname
+    rObj.courseId = items._id
     courseList.value.push(rObj)
   })
   console.log('所有课程信息', courseList)
 }
 
-// 查询学生,para:班级编号bid
-const getParams = async (val) => {
-  params.value = val.slice(4)
-  console.log('params参数', params.value)
-  // const result = await getStudents(val.slice(0, 4))
-  // console.log('查询学生的信息', result.data)
-  //   let rObj = []
-  //   rObj = result.data.sids
-  //   console.log(rObj)
-  //   rObj.map(function (obj) {
-  //   const rObj2 = {}
-  //   rObj2.sid = obj.sid
-  //   rObj2.name = obj.sname
-  //   rObj2.bid = obj.bid
-  //   rObj2.major = obj.major
-  //   tableData.push(rObj2)
-  // })
-}
+const studentsTable = reactive([])
 
 const queryStudents = async () => {
-  tableData.splice(0, tableData.length)
-  if (params.value) {
-    const result = await getStudentsScore(params.value)
-    console.log('查询学生的分数信息', result.data)
-    let rObj = []
-    rObj = result.data
-    // console.log(rObj)
-    rObj.map(function (obj) {
-      const rObj2 = {}
-      rObj2.sid = obj.student.sid
-      rObj2.name = obj.student.sname
-      rObj2.bid = obj.class.bid
-      rObj2.major = obj.class.major
-      // rObj2.oldScore = obj.score
-      rObj2.newScore = obj.score
-      rObj2.id = obj.id
-      tableData.push(rObj2)
-      console.log('tacleData', tableData)
+  // console.log('courseList.value.courseId', courseList.value.courseId)
+  studentsTable.splice(0, studentsTable.length)
+  if (courseList.value.courseId) {
+    const { data } = await getStudentsScore(courseList.value.courseId)
+    console.log('查询学生的分数信息', data)
+    data.forEach(function (items) {
+      const obj = {}
+      obj.sid = items.student.sid
+      obj.name = items.student.sname
+      obj.bid = items.class.bid
+      obj.major = items.class.major
+      obj.newScore = items.score
+      obj.id = items.id
+      studentsTable.push(obj)
+      console.log('studentsTable', studentsTable)
     })
     ElMessage({
       duration: 800,
@@ -132,17 +98,18 @@ const queryStudents = async () => {
     })
   }
 }
-// 提交分数,params:tableData.id
+
+const ScoreData = reactive([])
 const submitScore = async () => {
-  tableData.forEach((items) => {
+  studentsTable.forEach((items) => {
     const obj = {}
     obj.sid = items.sid
     obj.score = items.newScore
-    data.push(obj)
+    ScoreData.push(obj)
   })
-  const id = tableData[1].id
+  const id = studentsTable[1].id
   console.log(id)
-  const result = await postSubmitScore(id, data)
+  const result = await postSubmitScore(id, ScoreData)
   if (result.errno !== 0) {
     ElMessage({
       duration: 800,
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,17 +1,10 @@
 <template>
   <div>
-    <el-select v-model="options.value" class="m-2" placeholder="教师名称" size="large" @change="reqCourse">
-      <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
-    </el-select>
-    <el-select v-model="calendarTypeOptions.value" placeholder="课程名称" size="large" clearable class="filter-item"
-      style="margin-left: 8px;width: 180px">
-      <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.label" :value="item.value" />
-    </el-select>
     <el-button type="primary" style="margin-left:10px" @click="handleFilter" size="large">获取课程列表</el-button>
   </div>
   <div style="padding-top:15px">
     <el-card>
-      <el-table :data="tableData" border style="width: 100%" stripe>
+      <el-table :data="courseTableData" border style="width: 100%" stripe>
         <el-table-column prop="cid" label="课程编号" width="150" />
         <el-table-column prop="cname" label="课程名称" width="230" />
         <el-table-column prop="xueshi" label="学时" width="180" />
@@ -29,60 +22,25 @@
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue'
+import { reactive } from 'vue'
 import { getAllCourses } from '../../api/class'
-import { getTeachers, getTeacherCourse } from '../../api/teacher'
-
-const options = ref([])
-const calendarTypeOptions = ref([])
-const tableData = reactive([])
-
-const getTeacherList = async () => {
-  const result = await getTeachers()
-  // options.push(list.data)
-  result.data.map(function (obj) {
-    const rObj = {}
-    rObj.value = obj.tid
-    rObj.label = obj.tname
-    options.value.push(rObj)
-  })
-}
-getTeacherList()
 
 // 查询教师对应的课程
-const reqCourse = async (val) => {
-  calendarTypeOptions.value.splice(0, calendarTypeOptions.value.length)
-  const result = await getTeacherCourse(val.toString())
-  // console.log(result.data)
-  result.data.map(function (obj) {
-    const rObj = {}
-    // 班级编号
-    rObj.value = obj.class.bid + obj._id
-    // 课程名称
-    rObj.label = obj.course.cname
-    // 课程唯一id
-    rObj.cid = obj.course.cid
-    // 排课表的id
-    rObj.id = obj._id
-    calendarTypeOptions.value.push(rObj)
-  })
-  console.log('所有课程信息', calendarTypeOptions)
-}
 // 获取列表
+const courseTableData = reactive([])
 const getList = async () => {
-  const result = await getAllCourses()
-  let rObj = []
-  rObj = result.data
-  rObj.map(function (obj) {
-    const rObj2 = {}
-    rObj2.cid = obj.cid
-    rObj2.cname = obj.cname
-    rObj2.xueshi = obj.xueshi
-    rObj2.xuefen = obj.xuefen
-    rObj2.createdAt = obj.createdAt
-    tableData.push(rObj2)
-    console.log('tableData', tableData)
-  })
+  const { data } = await getAllCourses()
+  // data.forEach(function (obj) {
+  //   const rObj2 = {}
+  //   rObj2.cid = obj.cid
+  //   rObj2.cname = obj.cname
+  //   rObj2.xueshi = obj.xueshi
+  //   rObj2.xuefen = obj.xuefen
+  //   rObj2.createdAt = obj.createdAt
+  //   courseTableData.push(rObj2)
+  //   // console.log('courseTableData', courseTableData)
+  // })
+      courseTableData.push(data)
 }
   getList()
 const handleFilter = () => {
